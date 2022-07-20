58fb1b1eb85d1b099e2a03d966bd522891726db7
Matthew Kitchin <mkitchin@Matthews-MBP-2.localdomain>
2022-07-20 10:03:20 +1000

    initialise repository

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace a97enig.Crypto.Prices
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            System.Console.WriteLine("This is a placeholder.");
+        }
+    }
+}
